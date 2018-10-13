af28408d7f1e805a6e4ee25a1e7106ac938c87de
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2018-10-13 06:55:19 -0700

    working function

--- handler.js
+++ handler.js
@@ -1,3 +1,3 @@
-const serverless = require('@probot/serverless-lambda')
+const { serverless } = require('@probot/serverless-lambda')
 const appFn = require('./')
 module.exports.probot = serverless(appFn)
