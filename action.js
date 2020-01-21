54cb45b5f05d6da6110a98156031c8a08af2900e
Brian Douglas <bdougie@users.noreply.github.com>
2020-01-21 10:41:05 -0800

    adds GitHub Action probot adapter

---
+++ action.js
@@ -0,0 +1,9 @@
+// Require the adapter
+const adapt = require('probot-actions-adapter');
+
+// Require your Probot app's entrypoint, usually this is just index.js
+const probot = require('./index');
+
+// Adapt the Probot app for Actions
+// This also acts as the main entrypoint for the Action
+adapt(probot);
